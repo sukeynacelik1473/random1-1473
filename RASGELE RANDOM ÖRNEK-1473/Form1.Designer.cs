8c731b831af83c85801725abb80be368f805eac6
sukeynacelik1473 <145451091+sukeynacelik1473@users.noreply.github.com>
2023-10-24 14:14:40 +0300

    Add files via upload

---
+++ RASGELE RANDOM ÖRNEK-1473/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace RASGELE_RANDOM_ÖRNEK_1473
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUret = new System.Windows.Forms.Button();
+            this.lbNotlar = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnUret
+            // 
+            this.btnUret.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUret.Location = new System.Drawing.Point(13, 44);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(78, 52);
+            this.btnUret.TabIndex = 0;
+            this.btnUret.Text = "ÜRET";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // lbNotlar
+            // 
+            this.lbNotlar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbNotlar.FormattingEnabled = true;
+            this.lbNotlar.ItemHeight = 16;
+            this.lbNotlar.Location = new System.Drawing.Point(97, 44);
+            this.lbNotlar.Name = "lbNotlar";
+            this.lbNotlar.Size = new System.Drawing.Size(134, 388);
+            this.lbNotlar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(284, 448);
+            this.Controls.Add(this.lbNotlar);
+            this.Controls.Add(this.btnUret);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUret;
+        private System.Windows.Forms.ListBox lbNotlar;
+    }
+}
+
---
+++ RASGELE RANDOM ÖRNEK-1473/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RASGELE_RANDOM_ÖRNEK_1473
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+
+           Random rnd=new Random();
+            lbNotlar.Items.Clear();
+            for (int i = 0; i <= 14; i++) 
+            { 
+                int puan= rnd.Next(0,100);
+                lbNotlar.Items.Add(puan);
+            }
+
+
+           
+
+        }
+    }
+}
